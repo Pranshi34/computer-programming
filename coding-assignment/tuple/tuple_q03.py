817af92361575811140ff07a8de34aeac01860b8
Pranshi34 <prashant14708@gmail.com>
2024-11-28 21:05:05 +0530

    Create tuple_q03.py

---
+++ coding-assignment/tuple/tuple_q03.py
@@ -0,0 +1,8 @@
+# Convert a tuple of strings to a single string.
+
+def tuple_to_single_string(tup):
+    return ''.join(tup)
+
+tup = ('H', 'e', 'l', 'l', 'o')
+result = tuple_to_single_string(tup)
+print(result)
