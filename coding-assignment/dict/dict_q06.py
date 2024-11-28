68d6c8ec8c55cf41bced196660cbf8cfbfa80ecf
Pranshi34 <prashant14708@gmail.com>
2024-11-28 20:31:58 +0530

    Create dict_q06.py

---
+++ coding-assignment/dict/dict_q06.py
@@ -0,0 +1,6 @@
+# Write a Python program to iterate over dictionaries using for loops.
+
+
+data = {'a': 1, 'b': 2, 'c': 3, 'd': 4}
+for key, value in data.items():
+    print(f"Key: {key}, Value: {value}")
