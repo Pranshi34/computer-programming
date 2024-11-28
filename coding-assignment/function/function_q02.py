da2b63715c5390585ee2859a19267a3a67ac7fa3
Pranshi34 <prashant14708@gmail.com>
2024-11-28 21:16:27 +0530

    Create function_q02.py

---
+++ coding-assignment/function/function_q02.py
@@ -0,0 +1,5 @@
+# Write a Python function to find the longest word in a list of strings.
+
+def longest_word(words):
+    return max(words, key=len)
+print(longest_word(["apple", "banana", "cherry"]))
